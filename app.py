87157dd0e449094ab7748588a3085722fac3af6e
Andrew <e1398281@u.nus.edu>
2025-10-18 22:21:50 +0800

    chore: minor changes

--- app.py
+++ app.py
@@ -142,8 +142,7 @@ col1, col2, col3 = st.columns([1, 1, 1])
 
 with col2:
     if st.button("Let's Start", type="primary", use_container_width=True):
-        st.markdown("**Redirecting to Chat page...**")
-        st.markdown("Please click on '💬 Chat' in the sidebar to continue.")
+        st.switch_page("pages/1_💬_Chat.py")
 
 st.markdown('</div>', unsafe_allow_html=True)
 
